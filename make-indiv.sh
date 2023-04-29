6e1e37195920eeb560aafdd8f768a8174a7d12ec
Philip Chung <philipchung1995@yahoo.com>
2023-04-29 00:54:04 -0700

    Add make-indiv.sh script into version control

---
+++ make-indiv.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+	echo 'Need a name'
+	exit 1
+fi
+
+BASE_NAME="$1"
+OLD_RESUME_NAME='resume.tex'
+NEW_RESUME_NAME='philip chung.tex'
+
+DIR_NAME="_$BASE_NAME"
+
+if [ -e "$DIR_NAME" ]; then
+	echo "$DIR_NAME exists"
+	exit 1
+fi
+
+mkdir "$DIR_NAME"
+
+cp "$OLD_RESUME_NAME" "$DIR_NAME/$NEW_RESUME_NAME"
